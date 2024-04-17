a29f742c83bfadb80001a232d7828f78e84ae16f
Manjot Singh Gill <manjotg6@gmail.com>
2024-04-17 17:13:19 +0530

    linked-list

--- src/linked-list/double-linked-list.cpp
+++ src/linked-list/double-linked-list.cpp
@@ -22,14 +22,48 @@ public:
     }
 };
 
-template<class T>
-class DoubleLinkedList{
-    protected:
+template <class T>
+class DoubleLinkedList
+{
+protected:
     Node *head, *tail;
-    public:
-    DoubleLinkedList(){
+
+public:
+    DoubleLinkedList()
+    {
         head = tail = nullptr;
     }
 
-    
+    void addToTail(const T &);
+    T deleteFromTail();
 };
+
+template <class T>
+void DoubleLinkedList<T>::addToTail(const T &val)
+{
+    if (tail != nullptr)
+    {
+        tail = new Node<T>(val, nullptr, tail);
+        tail->prev->next = tail;
+    }
+    else
+        head = tail = new Node<T>(val);
+}
+
+template <class T>
+T DoubleLinkedList<T>::deleteFromTail()
+{
+    T val = tail->data;
+    if (head == tail)
+    {
+        delete head;
+        head = tail = nullptr;
+    }
+    else
+    {
+        tail = tail->prev;
+        delete tail->next;
+        tail->next = nullptr;
+    }
+    return val;
+}
